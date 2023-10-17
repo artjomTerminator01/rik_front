e9403b5f0511df4bc8025103b908077348d30e4a
Artjom Gussev <artjomgussev2001@gmail.com>
2023-10-18 00:15:26 +0300

    update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,17 +2,24 @@ import React from "react";
 import styles from "./navbar.module.scss";
 import classNames from "classnames";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  const location = useLocation();
+
   return (
     <div className={styles.wrapper}>
       <div className={classNames("container", styles.linksWrapper)}>
-        <Link className={styles.link} to="/">
-          Home
-        </Link>
-        <Link className={styles.link} to="/create-company">
-          Create company
-        </Link>
+        {location.pathname !== "/" && (
+          <Link className={styles.link} to="/">
+            Home
+          </Link>
+        )}
+        {location.pathname !== "/create-company" && (
+          <Link className={styles.link} to="/create-company">
+            Create company
+          </Link>
+        )}
       </div>
     </div>
   );
